4aebc9c6fd8c417ee70ff7b428ccf9c2a39e25c6
Ziad <ziad10a@live.com>
2020-09-08 19:59:34 +0400

    Health Bar + Life Count

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Image fillBar;
+    public float health;
+
+    //100 health => 1 fill amount
+    //45 health => 0.45 fill amount
+
+    public void LoseHealth(int value)
+    {
+        //Do nothing if you are out of health
+        if (health <= 0)
+            return;
+        //Reduce the health 
+        health -= value;
+        //Refresh the UI fillBar
+        fillBar.fillAmount = health / 100;
+        //Check if your health is zero or less => Dead
+        if(health<=0)
+        {
+            Debug.Log("YOU DIED");
+        }
+    }
+    private void Update()
+    {
+        //if (Input.GetKeyDown(KeyCode.Return))
+        //    LoseHealth(25);
+    }
+}
---
+++ Assets/Scripts/LifeCount.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LifeCount : MonoBehaviour
+{
+    public Image[] lives;
+    public int livesRemaining;
+
+    //4 lives - 4 imgaes (0,1,2,3)
+    //3 lives - 3 images (0,1,2,[3])
+    //2 lives - 2 images (0,1,[2],[3])
+    //1 life - 1 image (0,[1],[2],[3])
+    //0 lives - 0 images ([0,1,2,3]) LOSE
+
+    public void LoseLife()
+    {
+        //If no lives remaining do nothing
+        if (livesRemaining == 0)
+            return;
+        //Decrease the value of livesRemaining
+        livesRemaining--;
+        //Hide one of the life images
+        lives[livesRemaining].enabled = false;
+
+        //If we run out of lives we lose the game
+        if(livesRemaining==0)
+        {
+            Debug.Log("YOU LOST");
+        }
+    }
+
+    private void Update()
+    {
+        //if (Input.GetKeyDown(KeyCode.Return))
+        //    LoseLife();
+    }
+
+}
